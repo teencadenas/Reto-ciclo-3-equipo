4f4ce9c29bfaa3fed71699bbd68bfc84b4c09b57
teencadenas <arcadena91@misena.edu.co>
2021-09-20 19:42:12 -0500

    primer commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Buses{
+        public int id {get;set;}
+        public string marca{get;set;}
+        public int modelo{get;set;}
+        public int kilometraje{get;set;}
+        public int numeroAsientos{get;set;}
+        public string placa{get;set;}
+    }
+}
---
+++ ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Estaciones {
+    public int id{get;set;}
+    public string nombre{get;set;}
+    public string direccion{get;set;}
+    public float coordenandaX{get;set;}
+    public float coordenadaY{get;set;}
+    public string tipo{get;set;}
+    }
+}
